cb40aa57659507224856ed52b68036dc40ed94ec
raihanandhikasaragih <raihanadhikasaragih@student.uir.ac.id>
2023-05-20 20:07:40 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+   <h2>Tentang Saya</h2>
+   <p>RAIHAN ANDHIKA SARAGIH</p>
+   <p>213510832</p>
+   <p>Teknik Informatika</p>
+   <h3>Universitas Islam Riau</h3>
+  </main>
+</template>
+<style>
+  h2{
+    text-align : center;
+    font-size: 25px;
+    font-weight: both;
+    margin: 20px 0;
+    text-transform: uppercase;
+    color: whitesmoke;
+  }
+  h3{
+    text-align : center;
+    font-size: 15px;
+    font-weight: both;
+    margin: 20px 0;
+    text-transform: uppercase;
+    color: white;
+
+  }
+  p{
+    text-align: center;
+    font-size: 10px;
+    margin: auto;
+    color: white;
+  }
+</style>
